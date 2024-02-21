4873aa5d617eb401a4c7d8169fb77595626be97b
treyh12 <159801684+treyh12@users.noreply.github.com>
2024-02-21 13:58:30 -0500

    Input and Conditions

---
+++ conditions.py
@@ -0,0 +1,22 @@
+# CS104
+
+# Trey Harpootlian
+
+# conditions.py
+
+print("Please enter the current temperature:")
+
+if ("the temperature is > than 90 degrees:")
+    print("Wear shorts")
+
+if ("the temperature is > than 70 degrees:")
+    print ("short sleeves are fine")
+
+if ("the temperature is greater than 50 degrees:")
+    print ("wear a jacket")
+
+if ("the temperature is greater than 32 degrees:")
+    print ("Wear a heavy coat")
+
+else:
+    print ("Stay inside")
