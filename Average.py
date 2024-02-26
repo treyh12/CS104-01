2f094f6837f271e7031c5a871cff6b360352c582
treyh12 <159801684+treyh12@users.noreply.github.com>
2024-02-26 10:06:04 -0500

    Score Average Program with While Loop

---
+++ Average.py
@@ -0,0 +1,26 @@
+#CS 104
+
+#Trey Harpootlian
+
+#Averaging Test Scores
+
+#average.py
+
+#Set Variables
+average = 0
+total = 0
+howManyEntered = 0
+
+#Set how many scores to average
+howMany = int(input("How many test scores would you like to average?")
+
+#While loop to enter all test scores
+while (howManyEntered < howMany:
+        testScore = int(input("Enter Test Score:")
+        total = total + totalScore
+        howManyEntered = howManyEntered + 1
+
+#Compute and display average to user
+average = total/howMany
+print("The average for the test scores you entered is", average)
+                        
